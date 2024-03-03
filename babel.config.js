bb6f4ce287b2da8c2418faa3b40ce152acfbcd15
unknown <xlongclaw@gmail.com>
2024-03-03 14:10:14 +0530

    configured tailwind and added aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+          alias: {
+            "@screens/*": "./src/screens/*",
+            "@containers/*": "./src/containers/*",
+            "@components/*": "./src/components/*",
+          },
+        },
+      ],
+    ],
   };
 };
